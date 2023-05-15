0985dd5d6c991235619c7bc1422e29331c9dda68
david-10dn <davnev0423@skola.goteborg.se>
2023-05-15 19:11:21 +0200

    changed navBar style

--- src/components/NavPage.vue
+++ src/components/NavPage.vue
@@ -5,9 +5,11 @@ import { RouterLink } from 'vue-router'
 <template>
   <div>
     <div class="nav">
-      <RouterLink to="/">Home</RouterLink> <br>
-      <RouterLink to="/category"> Categories</RouterLink> <br>
-      <RouterLink to="/About"> About </RouterLink>
+      <ul>
+        <li><a class="activ" href="#news"><RouterLink to="/">Home</RouterLink></a></li>
+        <li><a href="#category"><RouterLink to="/category"> Categories</RouterLink></a></li>
+        <li style="float:right"><a href="#about"><RouterLink to="/About"> About </RouterLink></a></li>
+      </ul>
     </div>
   </div>
 </template>
@@ -15,13 +17,43 @@ import { RouterLink } from 'vue-router'
 <style scoped>
 body {
   color: white;
-  
+
 }
-.nav {
+
+/* .nav {
   display: flex;
   list-style-type: none;
   margin: .5rem;
   padding: 1rem;
   justify-content: space-between;
+} */
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+  border-right:1px solid #bbb;
+}
+
+li:last-child {
+  border-right: none;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
 }
 </style>

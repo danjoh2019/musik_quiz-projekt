99a60ba8074c0102192cfbb49f0598dd558376c2
Tilda Kampman <tilda.kampman@gmail.com>
2023-05-15 16:31:36 +0200

    add fonts and change color of category buttons

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -5,14 +5,12 @@
             <div class="header-category">
                 <h2>Categories</h2>
             </div>
-            <div class="background">
-                <div class="content-category">
-                    <div class="container-category-buttons">
-                        <button v-for="category in categories" :key="category.id" :value="category.genre" :id="category.id"
-                            @click="selectCategory(category.id, category.genre)" class="category-button">
-                            <p>{{ category.genre }}</p>
-                        </button>
-                    </div>
+            <div class="content-category">
+                <div class="container-category-buttons">
+                    <button v-for="category in categories" :key="category.id" :value="category.genre" :id="category.id"
+                        @click="selectCategory(category.id, category.genre)" class="category-button">
+                        <p>{{ category.genre }}</p>
+                    </button>
                 </div>
             </div>
         </div>
@@ -28,21 +26,22 @@ function selectCategory(id, categoryString) {
 }
 </script>
 <style scoped>
-
-.song-container {
-    color: white;
+.header-category {
+    font-family: Montserrat, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    text-transform: uppercase;
+    text-align: center;
 }
 
-.background {
+.container-category {
     align-items: center;
 }
 
-.container-category-buttons,
-.content-category {
+.container-category-buttons {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     justify-content: space-around;
+
 }
 
 .category-button {
@@ -52,43 +51,44 @@ function selectCategory(id, categoryString) {
     width: 8rem;
     height: 6rem;
     padding: 5rem;
-    border-radius: 1rem; 
+    border-radius: 1rem;
     margin: 0.5rem;
     border: none;
     background-color: lightblue;
-    font-family:  Montserrat, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-family: Montserrat, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     font-weight: bold;
     font-size: .8rem;
     letter-spacing: 0.3rem;
     text-transform: uppercase;
-    cursor: pointer; 
-
+    cursor: pointer;
 }
+
 .category-button p {
     padding: 2rem;
     color: white;
-    
-   
 }
 
 button {
     display: flex;
     justify-content: center;
     align-items: center;
+
 }
 
 .category-button:hover {
-    background-color:grey;
+    background-color: grey;
     color: white;
 }
 
 @media screen and (min-width: 560px) {
-
-    .container-category-buttons,
-    .content-category {
+    .container-category-buttons {
         display: grid;
-        grid-template-columns: auto auto;
+        grid-template-columns: repeat(3, 1fr);
+        justify-items: center;
+
     }
+
+
 
     .category-button {
         width: 14rem;
@@ -98,16 +98,54 @@ button {
 
 @media screen and (min-width: 750px) {
 
-.container-category-buttons,
-.content-category {
-    display: grid;
-    grid-template-columns: auto auto auto;
-}
+    .container-category-buttons {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        justify-items: center;
 
-.category-button {
-    width: 14rem; 
-   
-}
-}
+    }
 
+    .category-button {
+        width: 18rem;
+        height: 10rem;
+        font-size: 1rem;
+        background: linear-gradient(180deg, #4E70D3 0%, #2431A4 100%);
+        box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
+        border-radius: 10px;
+    }
+
+    button[value="pop"],
+    button[value="folkmusik"],
+    button[value="60s/70s"]
+     {
+
+        background: rgba(253, 145, 145, 0.7);
+        box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
+        border-radius: 10px;
+
+    }
+
+    button[value="elektroniskt"] {
+
+        background: linear-gradient(180deg, #4E70D3 0%, #2431A4 100%);
+        box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
+        border-radius: 10px;
+
+    }
+
+    button[value="elektroniskt"] ,
+    button[value="soul/r&b"]
+    {
+        background: linear-gradient(180deg, rgba(235, 66, 66, 0.9) 0%, rgba(204, 3, 39, 0.9) 100%);
+        box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
+        border-radius: 10px;
+    }
+
+    button[value="dansband"] ,
+    button[value="klassiskt"] {
+        background: linear-gradient(180deg, rgba(254, 164, 24, 0.9) 0%, rgba(255, 153, 33, 0.9) 100%);
+        box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
+        border-radius: 10px;
+    }
+}
 </style>

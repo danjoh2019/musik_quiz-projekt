23cd53e53564149efcb09e1ed2e2073583185e05
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-05-16 08:34:33 +0200

    added popup window

--- src/components/NavPage.vue
+++ src/components/NavPage.vue
@@ -12,9 +12,37 @@ import { RouterLink } from 'vue-router'
       </div>
 
 
-        <RouterLink to="/">Start</RouterLink>
-        <RouterLink to="/category"> Quiz</RouterLink>
-        <RouterLink to="/About"> Om Oss</RouterLink>
+      <RouterLink to="/">Start</RouterLink>
+      <RouterLink to="/category"> Quiz</RouterLink>
+      <!-- <RouterLink to="/About"> Om Oss</RouterLink> -->
+
+      <a class="button" href="#popup1">Om oss</a>
+
+      <div id="popup1" class="overlay">
+        <div class="popup">
+          <h2> Om oss </h2>
+          <a class="close" href="#">&times;</a>
+          <div class="content">
+          <p>
+            Welcome to our music quiz app, where you can test your music expertise and patience
+            as you quess your way to the top. How many answers will you get right? <br>
+            May the odds ever be in you favour!
+          </p>
+
+          <p>
+            This app was made by Daniel, Tilda, David and Christopher as a schoolproject. We decided to make a music app
+            beacuse
+            it sounded like a splendid idea. The app pulls data from the Sveriges Radio API, which means that you'll have
+            access to a large amount of information
+            about songs that have been played on the radio. This data is displayed in the app to help you test your music
+            knowledge.
+            As a starting point the app shows which 5 songs that were played the most from
+            three different time periods. More information will be added in the future!
+
+          </p>
+        </div>
+        </div>
+      </div>
 
 
 
@@ -50,7 +78,7 @@ a {
   color: #303BA1;
   font-size: .8rem;
   margin: .9rem;
-  
+
 
 }
 
@@ -61,6 +89,58 @@ a:hover {
 /*.nav a:last-child{
   margin-left: auto;
 }*/
+
+/* POPUP */
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+  transition: opacity 500ms;
+  visibility: hidden;
+  opacity: 0;
+}
+.overlay:target {
+  visibility: visible;
+  opacity: 1;
+}
+
+.popup {
+  margin: 70px auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  width: 30%;
+  position: relative;
+  transition: all 5s ease-in-out;
+}
+
+.popup h2 {
+  margin-top: 0;
+  color: #333;
+  font-family: Tahoma, Arial, sans-serif;
+}
+.popup .close {
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  transition: all 200ms;
+  font-size: 30px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #333;
+}
+.popup .close:hover {
+  color: #06D85F;
+}
+.popup .content {
+  max-height: 30%;
+  overflow: auto;
+}
+
+/* POPUP */
 
 @media screen and (min-width: 560px) {
 
@@ -115,6 +195,13 @@ a:hover {
   li a:hover:not(.active) {
     background-color: #111;
   }*/
+
+  .box{
+    width: 70%;
+  }
+  .popup{
+    width: 70%;
+  }
 
 }
 </style>

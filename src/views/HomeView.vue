965559492e582e169aa1780cbe8a92bd9efa319a
david-10dn <davnev0423@skola.goteborg.se>
2023-05-16 09:30:27 +0200

    added logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,23 +4,32 @@ import TopLists from '../components/TopLists.vue';
 </script>
 
 <template>
-    <main>
-        <div class="wrapper">
-          <div class="header">
-          
-            MUSIKQUIZ
-          </div>
-    
-          <div class="content">
-           
-            <TopLists />
-    
-          </div>
-          <div class="footer">
-            
-                  2023
-              </div>
-        </div>
-    
-      </main>
+  <main>
+    <div class="wrapper">
+      <div class="header"> 
+        <div class="header-logo"><img src="../assets/img/logo-color.png" width="200" height="66" alt="musicquiz logo"></div>
+
+      </div>
+
+      <div class="content">
+
+        <TopLists />
+
+      </div>
+      <div class="footer">
+
+        2023
+      </div>
+    </div>
+
+  </main>
 </template>
+
+<style>
+#title {
+  color: red;
+  font-family: "Montserrat", Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  text-align: center;
+  font-size: 30px;
+}
+</style>

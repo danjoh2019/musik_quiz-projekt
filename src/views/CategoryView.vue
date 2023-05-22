c3ef2fee9cd6e6589b96b96d5341e8557fdd7e97
Tilda Kampman <tilda.kampman@gmail.com>
2023-05-22 13:27:41 +0200

    clean spacing in categoryView

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -2,7 +2,6 @@
 <template>
     <div class="container-category">
         <div class="header-category">
-            <!--<h1>Quiz</h1>-->
             <div class="header-img">
                 <img src="../assets/img/dancing.jpg" alt="illustration of people dancing" />
             </div>
@@ -24,34 +23,29 @@ function selectCategory(id, categoryString) {
     router.push({ name: 'quiz', params: { id: id }, query: { genre: categoryString } })
 }
 </script>
+
 <style scoped>
-/* ---------------- FOR SMALLEST SCREEN --------------*/
 .container-category {
     text-transform: uppercase;
     text-align: center;
-    
     display: grid;
     grid-template-rows: 8rem 1fr;
 
 }
-
 .header-img img {
     width: 100%;
     transform: scale(3.5);
     position: fixed;
     z-index: -1;
     right: 16rem;
-    top: 11rem;   
+    top: 11rem;
 }
-
 .container-category-buttons {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     justify-items: center;
     background: linear-gradient(180deg, #fcf9f914 0%, #f0f6f9ef 15%, #fffffff0 100%);
-
 }
-
 .category-button {
     width: 10rem;
     padding: 3rem;
@@ -71,67 +65,45 @@ button {
     display: flex;
     justify-content: center;
     align-items: center;
-
 }
-
-/* -------- BUTTONS ----------*/
 
 button[value="elektroniskt"],
 button[value="pop"],
 button[value="hiphop"] {
     background: linear-gradient(180deg, #f8c8c8 0%, #FFADAD 100%);
     box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-
 }
-
 
 button[value="dansband"],
 button[value="60/70-tal"],
 button[value="country"] {
-
     background: linear-gradient(180deg, #4e6fd3 0%, #2431A4 100%);
     box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-
 }
-
 
 button[value="folkmusik"],
 button[value="soul/r&b"] {
     background: linear-gradient(180deg, #eb4242 0%, #CC0327 100%);
     box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-
-
 }
 
 button[value="jazz"],
 button[value="klassiskt"] {
     background: linear-gradient(180deg, #fea618 0%, #FF9921 100%);
     box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-
-
 }
 
-/* -------- BUTTONS END----------*/
-
-
-
-/* ---------------- FOR SMALL SCREEN --------------*/
-
 @media screen and (min-width: 576px) {
-
     .header-img img {
-       
         transform: scale(2);
         right: 20rem;
         top: 13rem;
     }
-
     .container-category-buttons {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         justify-items: center;
     }
-
     .category-button {
         width: 12rem;
         flex-grow: 1;
@@ -140,42 +112,33 @@ button[value="klassiskt"] {
         margin: 1rem;
     }
 
-    /* --------------- BUTTONS BIGGER SCREEN -------------*/
-
     button[value="jazz"],
     button[value="pop"],
     button[value="hiphop"] {
         background: linear-gradient(180deg, #F8C8C8 0%, #FFADAD 100%);
         box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-        
-
     }
 
     button[value="jazz"]:hover,
     button[value="pop"]:hover,
     button[value="hiphop"]:hover {
-        background: linear-gradient(180deg, #f8c8c8 0%,  #ffacac 50%,rgb(250, 131, 131) 100%);
+        background: linear-gradient(180deg, #f8c8c8 0%, #ffacac 50%, rgb(250, 131, 131) 100%);
         box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-        border-inline:1px solid  #f8c8c8 ;
-      
-
+        border-inline: 1px solid #f8c8c8;
     }
 
     button[value="folkmusik"],
     button[value="60/70-tal"],
     button[value="country"] {
-
         background: linear-gradient(180deg, #4E70D3 0%, #2431A4 100%);
         box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-
     }
 
     button[value="folkmusik"]:hover,
     button[value="60/70-tal"]:hover,
     button[value="country"]:hover {
         background: linear-gradient(180deg, #6187f8 0%, rgb(39, 53, 176) 100%);
-        border-inline:1px solid #6187f8;
-
+        border-inline: 1px solid #6187f8;
     }
 
     button[value="dansband"],
@@ -187,27 +150,21 @@ button[value="klassiskt"] {
     button[value="dansband"]:hover,
     button[value="soul/r&b"]:hover {
         background: linear-gradient(180deg, #f45151 0%, #a0011e 100%);
-        border-inline:1px solid #f45151;
+        border-inline: 1px solid #f45151;
     }
 
     button[value="elektroniskt"],
     button[value="klassiskt"] {
         background: linear-gradient(180deg, #FEA418 0%, #ff9721df 100%);
         box-shadow: 2px 2px 8px rgba(57, 56, 56, 0.5);
-        
     }
 
     button[value="elektroniskt"]:hover,
     button[value="klassiskt"]:hover {
         background: linear-gradient(180deg, #f8bc5a 0%, #f98705 100%);
-        border-inline:1px solid#f8bc5a;
+        border-inline: 1px solid#f8bc5a;
     }
-
-    /* --------------- BUTTONS BIGGER SCREEN END -------------*/
-
 }
-
-/* ---------------- MEDIUM --------------*/
 
 @media screen and (min-width: 768px) {
     .container-category {
@@ -217,46 +174,30 @@ button[value="klassiskt"] {
         text-transform: uppercase;
         text-align: center;
         overflow: hidden;
-
-
     }
-
     .header-img img {
         transform: scale(1.5);
         position: relative;
         right: 0rem;
         top: 5rem;
-
-
     }
-
-
     .category-button {
         flex-grow: 1;
         padding: 4rem;
         border-radius: 1rem;
         margin: 0.5rem;
     }
-
-
-
 }
 
-/* ---------------- LARGE --------------*/
-
 @media screen and (min-width: 992px) {
-
     .container-category {
         grid-template-rows: 30% 70%;
     }
-
     .header-img img {
         transform: scale(.95);
         top: 2rem;
         max-width: 100%;
-
     }
-
     .container-category-buttons {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
@@ -264,16 +205,11 @@ button[value="klassiskt"] {
         gap: 1rem;
         padding: 2rem;
         background: linear-gradient(180deg, #fcf9f9 0%, #fcf9f9e3 20%, #ffffff9e 100%);
-
     }
-
     .category-button {
         font-size: 1.1rem;
         letter-spacing: .3rem;
         text-shadow: #060b39;
-
     }
-
-
 }
 </style>

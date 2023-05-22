0e1c27f8f9986d62f8aa5c63e604773dc13628b1
Tilda Kampman <tilda.kampman@gmail.com>
2023-05-22 14:50:27 +0200

    error Component

---
+++ src/components/ErrorMsg.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <div class="error-box">
+            <p>{{ errorMessage }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+ export default{
+    name: 
+    "ErrorMsg",
+
+    props: {
+        errorMessage: String
+    }
+ }
+</script>
